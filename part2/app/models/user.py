de2cc913df621bc9e4abf17858e7e9cc7fc604d9
DANDEV2433 <alexdani3344@gmail.com>
2025-03-10 16:40:12 +0100

    Register the plugin within the Application and Update the User Model to Include Password Hashing

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,10 +1,13 @@
 from .basemodel import BaseModel
 import re
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 class User(BaseModel):
     emails = set()
 
-    def __init__(self, first_name, last_name, email, is_admin=False):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
@@ -12,6 +15,15 @@ class User(BaseModel):
         self.is_admin = is_admin
         self.places = []
         self.reviews = []
+        self.hash_password(password)
+    
+    def hash_password(self, password):
+    # Hashes the password before storing it
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+    # Verifies if the provided password matches the hashed password.
+        return bcrypt.check_password_hash(self.password, password)
     
     @property
     def first_name(self):
